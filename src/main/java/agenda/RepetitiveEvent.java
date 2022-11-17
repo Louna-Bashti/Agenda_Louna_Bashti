399f3a44906c11e23562cefd12691cfe5f6d6931
Louna Bashti <bashlodu59@gmail.com>
2022-11-17 11:57:40 +0100

    Class Event, RepetitiveEvent and Agenda implemented, class Event fully tested, class RepetitiveEvent half tested

--- src/main/java/agenda/RepetitiveEvent.java
+++ src/main/java/agenda/RepetitiveEvent.java
@@ -22,13 +22,13 @@ public class RepetitiveEvent extends Event {
      * </UL>
      */
 
-    private ChronoUnit frequency =
+    private ChronoUnit frequency;
     public RepetitiveEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency) {
         super(title, start, duration);
+        if(frequency == ChronoUnit.WEEKS || frequency == ChronoUnit.DAYS || frequency == ChronoUnit.MONTHS)
+        this.frequency = frequency;
+        else {throw new IllegalArgumentException("Fréquence non comprise dans les possibles");}
 
-
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
     }
 
     /**
@@ -36,18 +36,56 @@ public class RepetitiveEvent extends Event {
      *
      * @param date the event will not occur at this date
      */
+    ArrayList<LocalDate> exceptions = new ArrayList<LocalDate>();
     public void addException(LocalDate date) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        exceptions.add(date);
     }
+
+    public boolean IsInDay(LocalDate date) {
+
+        LocalDateTime end = getStart().plus(getDuration());
+        LocalDateTime nextDateStart = getStart();
+        LocalDateTime nextDateEnd = end;
+
+        boolean occurs = false;
+
+        if (exceptions.contains(date))
+        // la date est dans les exceptions, donc on n'a pas à chercher plus loin
+        {occurs = false;}
+        // on vérifie que la date est bien comprise dans l'évènement au début (gestion des overlapping events incluse)
+        else if (getStart().toLocalDate().isBefore(date) && end.toLocalDate().isAfter(date)) {occurs = true;}
+        else if (getStart().toLocalDate().isEqual(date) || end.toLocalDate().isEqual(date)) {occurs = true;}
+        else {
+            //on vérifie toutes les occurrences de l'évènement jusqu'à ce que celle vérifiée dépasse la date donnée :
+            //Si la date en fait partie, on arrête la boucle while et on rend true, si on dépasse sans que la date en fasse partie, on rend false
+
+                do {
+                    nextDateStart = nextDateStart.plus(1, frequency);
+                    nextDateEnd = nextDateEnd.plus(1, frequency);
+                    if (nextDateStart.toLocalDate().isBefore(date) && nextDateEnd.toLocalDate().isAfter(date))
+                    {
+                        occurs = true;
+                        break;
+                    }
+                    else if (nextDateStart.toLocalDate().isEqual(date) || nextDateEnd.toLocalDate().isEqual(date))
+                    {
+                        occurs = true;
+                        break;}
+
+                }
+                while (date.isAfter(nextDateStart.toLocalDate()));
+                }
+        return occurs;
+
+        }
+
 
     /**
      *
      * @return the type of repetition
      */
     public ChronoUnit getFrequency() {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");    
+    return this.frequency;
     }
 
 }
